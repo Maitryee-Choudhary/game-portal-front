960331ecf3f747b533327c82dea0e4392112a841
Tushar <tusharvamdoskar#gmail.com>
2021-06-30 23:02:57 +0530

    3

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import SignIn from './components/SignIn'
-
+import SignIn from './components/SignIn';
+import Gamepage from './components/Gamepage';
 function App() {
   return (
   
@@ -17,9 +17,16 @@ function App() {
             <p>Login</p>
 			<SignIn />
           </Route>
+		  
+		  <Route path="/game">
+            <p>Game Page</p>
+			<Gamepage />
+          </Route>
+		  
 		  <Route path="/">
             <Home />
           </Route>
+		  
 		  
         </Switch>
 		<Footer />
---
+++ src/components/Gamepage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Gamepage.css'
+
+function Gamepage(){
+	return(
+	<div className="gamemain">
+		<div className="rightGamepage">
+		<p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+		
+		<div className="gamebox">
+		
+			<div class="miniclip-game-embed" data-game-name="zomball" data-theme="5" data-width="600" data-height="800" data-language="en" margin="0px"><a href="https://www.miniclip.com/games/zomball/">Play Zomball</a></div>
+			<p >
+				<a href="https://www.miniclip.com/games/zomball/" target="_blank">Play Zomball</a> -
+				<a href="https://www.miniclip.com/games/genre-3/" target="_blank">More Sports games</a> -
+				<a href="https://www.miniclip.com/terms" target="_blank">Terms and Conditions</a> -
+				<a href="https://www.miniclip.com/privacy" target="_blank">Privacy Policy</a>
+			</p>
+		
+		
+		</div>
+		</div>
+		<div className="leftGamepage">
+		<p>BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB</p>
+		<div className="gamebox">	
+		</div>
+		
+		</div>
+	</div>
+  );
+}
+export default Gamepage;
